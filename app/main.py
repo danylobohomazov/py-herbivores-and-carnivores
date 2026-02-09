416efd8e1b05a00f071bc5c910c737f5197fd7ab
Danylo <danil.bogomazov1356@gmail.com>
2026-02-10 00:50:03 +0200

    Change

--- app/main.py
+++ app/main.py
@@ -1,16 +1,21 @@
-class Animal:
-    alive = []
+# flake8: noqa: E501
+from __future__ import annotations
 
-    def __init__(self, name: str,
-                 health: int = 100, hidden: bool = False) -> None:
+
+class Animal:
+    alive: list[Animal] = []
+
+    def __init__(self,
+                 name: str,
+                 health: int = 100,
+                 hidden: bool = False) -> None:
         self.name = name
         self.health = health
         self.hidden = hidden
         Animal.alive.append(self)
 
     def __repr__(self) -> str:
-        return "{" + (f"Name: {self.name}, Health: {self.health}, "
-                      f"Hidden: {self.hidden}") + "}"
+        return f"{{Name: {self.name}, Health: {self.health}, Hidden: {self.hidden}}}"
 
 
 class Herbivore(Animal):
